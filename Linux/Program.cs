cbe83df5c7d6f77685af4c7a672f0d542bf78375
Jeremy Lindsay <jeremy.lindsay@outlook.com>
2017-05-07 21:08:13 +0100

    Refactored structure to logically separate Linux and Windows versions.

---
+++ Linux/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace RaspberryPiCore
+{
+    class Program
+    {
+        [DllImport("libc.so.6", EntryPoint = "toupper")]
+        private static extern int CharUpper(int c);
+
+        static void Main(string[] args)
+        {
+            var textToChange = "Hello Internet of Things!";
+            var inputCharacterArray = textToChange.ToCharArray();
+ 
+            // array of chars to hold the capitalised text
+            var outputCharacterArray = new char[inputCharacterArray.Length];
+ 
+            for(int i = 0; i < inputCharacterArray.Length; i++) 
+            {
+                var charToByte = (byte)inputCharacterArray[i];
+                outputCharacterArray[i] = (char)CharUpper(charToByte);
+            }
+ 
+            Console.WriteLine($"Original text is {textToChange}");
+            Console.WriteLine($"Changed text is {new string(outputCharacterArray)}");
+        }
+    }
+}
