a0b851683254590dca8fd11ead58535012545346
Jeremy Lindsay <jeremylindsayni@users.noreply.github.com>
2017-05-07 21:13:11 +0100

    Removed hardcoded values

--- Linux/deploy-ubuntu.ps1
+++ Linux/deploy-ubuntu.ps1
@@ -3,6 +3,6 @@ dotnet build
 
 dotnet publish . -r ubuntu.16.04-arm
 
-& pscp.exe -r .\bin\Debug\netcoreapp2.0\ubuntu.16.04-arm\publish\* ubuntu@192.168.1.110:/home/ubuntu/rasppinvoke
+& pscp.exe -r .\bin\Debug\netcoreapp2.0\ubuntu.16.04-arm\publish\* <<remote username>>@<<machine ip address>>:/home/ubuntu/rasppinvoke
 
-& plink.exe -v -ssh ubuntu@192.168.1.110 chmod u+x,o+x /home/ubuntu/rasppinvoke/RaspberryPi_PInvoke
+& plink.exe -v -ssh <<remote username>>@<<machine ip address>> chmod u+x,o+x /home/<<remote username>>/rasppinvoke/RaspberryPi_PInvoke
